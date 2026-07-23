b922021f84986e1de905d2d282cc86ac1ac41652
adamskid <adamusman.bincom@gmail.com>
2026-07-23 11:41:53 +0100

    edited my axios interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -23,7 +23,6 @@ api.interceptors.response.use(
         if (error.response?.status === 401) {
             localStorage.removeItem("token");
 
-            window.location.href = "/login";
         }
 
         return Promise.reject(error);
