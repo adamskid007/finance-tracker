32c734402b1dc9bf0a882310ec095ff1dcec3f92
adamskid <adamusman.bincom@gmail.com>
2026-07-23 11:19:11 +0100

    edited jwt

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -5,7 +5,7 @@ export const generateToken = (userId) => {
         { id: userId },
         process.env.JWT_SECRET,
         {
-            expiresIn: process.env.JWT_EXPIRES_IN,
+            expiresIn: process.env.JWT_EXPIRES_IN || "7d",
         }
     );
 };
